f9e9cd0e8e8d593d6ad01ce1f886c7fac30399d0
Sam21sop <sbussiness21@gmail.com>
2024-01-29 18:12:43 +0530

    changed

--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,13 +1,10 @@
 import projectModel from "../models/projectModel.js";
 
-
 export const homeController = async (req, res) => {
   try {
     const projects = await projectModel.find();
-    // res.render("home");
     res.render("home", { projects });
   } catch (error) {
-    console.error(error);
     res.status(500).send("Internal Server Error");
   }
 };
